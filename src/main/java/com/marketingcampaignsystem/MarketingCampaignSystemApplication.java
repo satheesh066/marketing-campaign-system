707a45b519edfa2b63c3c900ae3988bb6c9b4fb9
Satheesh066 <satheesh.singanamala@gmail.com>
2025-01-29 22:43:13 -0600

    base spring boot project

---
+++ src/main/java/com/marketingcampaignsystem/MarketingCampaignSystemApplication.java
@@ -0,0 +1,13 @@
+package com.marketingcampaignsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingCampaignSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingCampaignSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marketingcampaignsystem/MarketingCampaignSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketingcampaignsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketingCampaignSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
